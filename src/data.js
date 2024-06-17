0bcf14b4b9090646e35961fc840feeeceeb878ff
it <kyle9742@naver.com>
2024-06-17 10:49:47 +0900

    네브바 메뉴, 아이콘 map으로 가져오기

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import tour1 from '/images/tour-1.jpeg';
+import tour2 from '/images/tour-2.jpeg';
+import tour3 from '/images/tour-3.jpeg';
+import tour4 from '/images/tour-4.jpeg';
+
+export const pageLinks = [
+    { id: 1, href: '#home', text: 'home'},
+    { id: 2, href: '#about', text: 'about'},
+    { id: 3, href: '#services', text: 'services'},
+    { id: 4, href: '#tours', text: 'tours'},
+    { id: 5, href: '#contact', text: 'contact'},
+];
+
+export const socialLinks = [
+    { id: 1, href: 'http://www.facebook.com', icon: 'fab fa-facebook' },
+    { id: 2, href: 'http://www.twitter.com', icon: 'fab fa-twitter' },
+    { id: 3, href: 'https://www.instagram.com/', icon: 'fab fa-instagram' },
+];
